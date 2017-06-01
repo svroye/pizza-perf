0dbaf251dbf44453ef73259ac4a91a21563e4f18
Van Roye <steven.van-roye@sogeti.com>
2017-06-01 21:54:24 +0200

    Change resizePizza to avoid FSL

--- js/main.js
+++ js/main.js
@@ -421,38 +421,27 @@ var resizePizzas = function(size) {
 
   changeSliderLabel(size);
 
-  // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  function determineDx (elem, size) {
-    var oldwidth = elem.offsetWidth;
-    var windowwidth = document.querySelector("#randomPizzas").offsetWidth;
-    var oldsize = oldwidth / windowwidth;
-
-    // Changes the slider value to a percent width
-    function sizeSwitcher (size) {
-      switch(size) {
-        case "1":
-          return 0.25;
-        case "2":
-          return 0.3333;
-        case "3":
-          return 0.5;
-        default:
-          console.log("bug in sizeSwitcher");
-      }
-    }
-
-    var newsize = sizeSwitcher(size);
-    var dx = (newsize - oldsize) * windowwidth;
-
-    return dx;
-  }
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    switch(size) {
+        case "1":
+         newsize = 25;
+         break;
+        case "2":
+          newsize = 33.33;
+          break;
+        case "3":
+          newsize = 50;
+          break;
+        default:
+          console.log("bug in sizeSwitcher");
+      }
+
+    var pizzaCont = document.querySelectorAll(".randomPizzaContainer");
+
+    for (var i = 0; i < pizzaCont.length; i++) {
+      pizzaCont[i].style.width = newsize + "%";
     }
   }
 
